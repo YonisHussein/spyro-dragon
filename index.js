8907f7986beb711680bac2e6aec2fc3965b78c33
YonisHussein <yonismhussein@gmail.com>
2022-07-17 19:38:18 -0400

    add engineer to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Engineer = require('./lib/Engineer');
 
 const teamArray = [];
 
@@ -35,5 +36,27 @@ const addManager = () =>{
 }
 
 const addEngineer = () => {
-    return inquiere.prompt
+    return inquiere.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the name of the engineer',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the email of the engineer'
+        }
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the Github of the engineer'
+        },
+    ])
+    .then (engineerInfo => {
+        const {name, id, email, github} = engineerInfo;
+        const engineer = new Engineer(name, id, email, github);
+        teamArray.push(engineer);
+        console.log(engineer);
+    });
 }
