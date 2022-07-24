9e617f771cb58dac6a9e1cd41846cc7594c15699
YonisHussein <yonismhussein@gmail.com>
2022-07-24 18:15:36 -0400

    last commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/intern');
+const Intern = require('./lib/Intern');
 
 const teamArray = [];
 
